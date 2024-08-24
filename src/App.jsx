b3242ad4ac7cbb48d8dba9195cc6ed89e5e63feb
Ashish Gaikwad <work.ashish.gaikwad@gmail.com>
2024-08-24 19:18:34 +0530

    fix minor issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import BlogList from "./components/blogs/BlogList";
 import PostFormModal from "./components/blogs/PostFormModal";
-import { handleOpenModal } from "./features/posts/postsSlice";
 
 function App() {
-  const dispatch = useDispatch();
   const selectedPost = useSelector((state) => state.posts.selectedPost);
 
   return (
     <div className="App">
-      <div className="container mx-auto px-4 py-8">
+      <div className="container mx-auto px-4 py-2">
         <PostFormModal post={selectedPost} />
       </div>
       <BlogList />
