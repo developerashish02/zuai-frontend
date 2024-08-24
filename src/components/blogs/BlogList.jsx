ba5ed38524e9bf6935912ecbf341739d3fd52d2b
Ashish Gaikwad <work.ashish.gaikwad@gmail.com>
2024-08-24 13:55:35 +0530

    add shimmer ui

--- src/components/blogs/BlogList.jsx
+++ src/components/blogs/BlogList.jsx
@@ -1,50 +1,34 @@
 import BlogCard from "./BlogCard";
 import { useGetAllPostsQuery } from "../../services/posts";
+import ShimmerCard from "../common/ShimmerCard";
 
-const dummyBlogs = [
-  {
-    id: 1,
-    title: "Exploring the Future of AI",
-    excerpt:
-      "Dive into the advancements and implications of artificial intelligence in various sectors.",
-    image: "https://via.placeholder.com/300/87CEEB/FFFFFF",
-    link: "#",
-  },
-  {
-    id: 2,
-    title: "A Journey Through Space",
-    excerpt: "Discover the latest findings and missions in space exploration.",
-    image: "https://via.placeholder.com/300/6A5ACD/FFFFFF",
-    link: "#",
-  },
-  {
-    id: 3,
-    title: "The Rise of Quantum Computing",
-    excerpt:
-      "Understanding quantum computing and its potential to revolutionize technology.",
-    image: "https://via.placeholder.com/300/FFB6C1/FFFFFF",
-    link: "#",
-  },
-];
 const BlogList = () => {
   const {
     data: postsData,
     isLoading,
-    error,
     isSuccess,
+    error,
   } = useGetAllPostsQuery();
 
-  //   console.log(posts, "❤️");
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold text-center text-gray-800 mb-8">
         Featured Blogs
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {isSuccess &&
-          postsData.posts.map((blog) => (
-            <BlogCard key={blog._id} blog={blog} />
-          ))}
+        {isLoading
+          ? Array.from({ length: 6 }).map((_, index) => (
+              <ShimmerCard key={index} />
+            ))
+          : isSuccess
+          ? postsData.posts.map((blog) => (
+              <BlogCard key={blog._id} blog={blog} />
+            ))
+          : error && (
+              <p className="text-center text-red-500">
+                Error loading blogs. Please try again later.
+              </p>
+            )}
       </div>
     </div>
   );
