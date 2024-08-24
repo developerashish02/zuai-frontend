43a47bab63bdd4ab52b0d01eb63a05630d630b28
Ashish Gaikwad <work.ashish.gaikwad@gmail.com>
2024-08-24 19:53:56 +0530

    add search functionality

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../common/Header";
+import Footer from "../common/Footer";
 
 const Layout = () => {
   return (
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
--- src/components/blogs/BlogList.jsx
+++ src/components/blogs/BlogList.jsx
@@ -1,8 +1,10 @@
+import React, { useState } from "react";
 import BlogCard from "./BlogCard";
 import { useGetAllPostsQuery } from "../../services/posts";
 import ShimmerCard from "../common/ShimmerCard";
 
 const BlogList = () => {
+  const [search, setSearch] = useState("");
   const {
     data: postsData,
     isLoading,
@@ -10,25 +12,44 @@ const BlogList = () => {
     error,
   } = useGetAllPostsQuery();
 
+  const filteredBlogs = isSuccess
+    ? postsData.posts.filter(
+        (blog) =>
+          blog.title.toLowerCase().includes(search.toLowerCase()) ||
+          blog.author.toLowerCase().includes(search.toLowerCase())
+      )
+    : [];
+
   return (
-    <div className="container mx-auto px-4 py-8 ">
+    <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold text-center text-gray-800 mb-8 font-mono">
         Featured Blogs
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ">
+
+      <div className="flex justify-center mb-8">
+        <input
+          type="text"
+          placeholder="Search for blogs by title or author"
+          onChange={(e) => setSearch(e.target.value)}
+          className="px-4 py-4 outline-none border border-black text-base w-2/3 rounded-lg"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {isLoading
           ? Array.from({ length: 6 }).map((_, index) => (
               <ShimmerCard key={index} />
             ))
-          : isSuccess
-          ? postsData.posts.map((blog) => (
-              <BlogCard key={blog._id} blog={blog} />
-            ))
+          : isSuccess && filteredBlogs.length > 0
+          ? filteredBlogs.map((blog) => <BlogCard key={blog._id} blog={blog} />)
           : error && (
               <p className="text-center text-red-500">
                 Error loading blogs. Please try again later.
               </p>
             )}
+        {!isLoading && isSuccess && filteredBlogs.length === 0 && (
+          <p className="text-center text-gray-500">No blogs found.</p>
+        )}
       </div>
     </div>
   );
