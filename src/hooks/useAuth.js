27568b042bdeae2dcd38a25ef172305e8cc11b49
Ashish Gaikwad <work.ashish.gaikwad@gmail.com>
2024-08-24 23:07:58 +0530

    fix issues

--- src/hooks/useAuth.js
+++
@@ -1,7 +0,0 @@
-const useAuth = () => {
-  const token = localStorage.getItem("token");
-
-  return token === null ? false : true;
-};
-
-export default useAuth;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { store } from "./app/store";
@@ -7,9 +6,6 @@ import App from "./App.jsx";
 import "./index.css";
 import Layout from "./components/Layout/Layout.jsx";
 import BlogDetails from "./components/blogs/BlogDetails.jsx";
-import LogIn from "./components/auth/SignupPage.jsx";
-import SignupPage from "./components/auth/SignupPage.jsx";
-import Protected from "./components/common/Protected.jsx";
 
 const appRoutes = createBrowserRouter([
   {
@@ -17,21 +13,12 @@ const appRoutes = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        path: "/login",
-        element: <SignupPage />,
+        path: "/",
+        element: <App />,
       },
       {
-        element: <Protected />,
-        children: [
-          {
-            path: "/",
-            element: <App />,
-          },
-          {
-            path: "/posts/:id",
-            element: <BlogDetails />,
-          },
-        ],
+        path: "/posts/:id",
+        element: <BlogDetails />,
       },
     ],
   },
