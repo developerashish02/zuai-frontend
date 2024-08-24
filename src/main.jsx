90e090e48a919511a2b075311d095ca43fedb1be
Ashish Gaikwad <work.ashish.gaikwad@gmail.com>
2024-08-24 14:10:54 +0530

    setup routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
+import Layout from "./components/Layout/Layout.jsx";
+
+const appRoutes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={appRoutes} />
   </Provider>
 );
